22258b5f1224ba5cd146b9025e0491beea6daf36
slim-33 <spenserlim3@gmail.com>
2025-11-16 10:39:13 -0800

    adjusted meters

--- src/components/Meter.jsx
+++ src/components/Meter.jsx
@@ -2,12 +2,17 @@ import "./Meter.css";
 
 function Meter(props) {
   return (
-    <div>
-      <div className="meter"
-        style={{ height: `${props.value * 5}px`,
-        backgroundColor: `${props.colour}`,
-        left: `${110 * props.place - 100}px` }}>
-        <h2>{props.type} Meter: {props.value}</h2>
+    <div className="meter-container" style={{ left: `${props.place * 220}px` }}>
+      <div
+        className="meter-bar"
+        style={{
+          width: `${props.value * 5}px`,
+          backgroundColor: props.colour,
+        }}
+      >
+        <span className="meter-text">
+          {props.type}: {props.value}
+        </span>
       </div>
     </div>
   );
